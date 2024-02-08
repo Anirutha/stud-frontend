133541343ec94783badf60de6a9ab41556891312
Anirutha <aniruthasivakumar03@gmail.com>
2024-02-08 09:25:05 +0530

    all changes done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { Route, Routes} from 'react-router-dom';
 import './App.css';
+import AddStudents from './Components/AddStudents';
+import DashBoard from './Components/DashBoard';
+import LoginPage from './Components/LoginPage';
+import Nopage from './Components/Nopage';
+import SignupPage from './Components/SignupPage';
+import Students from './Components/Students';
+import UpdateStudents from './Components/UpdateStudents';
+
 
 function App() {
+  const [students, setStudents] = useState([]);
+  
+  useEffect(()=>{
+    const getStudents = async () =>{
+        const response = await fetch("https://644b33bc4bdbc0cc3a8ce28c.mockapi.io/users", {
+          method:"GET",
+        }); 
+        const data = await response.json();
+        if(data){
+          setStudents(data)
+        }
+    }
+    
+      getStudents();
+    
+    
+  }, )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+
+<Route exact path="/"
+  element={<DashBoard/>} />
+
+<Route path="/login"
+  element={<LoginPage />}
+/>
+
+<Route path="/signup"
+  element={<SignupPage />}
+/>
+<Route path="/students"
+            element={<Students
+            students = {students}
+            setStudents ={setStudents}/>}
+            />
+          
+          <Route path="/addstudents"
+            element={<AddStudents
+            students = {students}
+            setStudents ={setStudents}/>}
+            />
+         
+         <Route path="/edit/:id"
+            element={<UpdateStudents
+              students = {students}
+              setStudents ={setStudents}/>}
+            />
+           
+           <Route path="**"
+              element={<Nopage/>}
+          />
+
+</Routes>
     </div>
   );
 }
